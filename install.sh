1438f468198dc94278b5d642df2219a49ca0af9b
fiathux <fiathux@gmail.com>
2023-02-15 18:36:09 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+check_toolchain() {
+}
+
+# do install
+install() {
+}
+
+# do remove
+uninstall() {
+}
+
+# show help document
+show_help() {
+  echo ""
+  echo "Usage of $0:"
+  echo ""
+  echo "    install sshcrab:"
+  echo "      $0 install [prefix]"
+  echo ""
+  echo "    remove sshcrab:"
+  echo "      $0 remove [prefix]"
+  echo ""
+}
